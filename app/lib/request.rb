3b4c6efa3f37d08cec7a4660025ca8c8d3dcd9ad
QueerNeko <QueerNeko@users.noreply.github.com>
2020-03-29 17:26:48 -0300

    Increase request body length limit to 2MB

--- app/lib/request.rb
+++ app/lib/request.rb
@@ -149,7 +149,7 @@ class Request
   end
 
   module ClientLimit
-    def body_with_limit(limit = 1.megabyte)
+    def body_with_limit(limit = 2.megabyte)
       raise Mastodon::LengthValidationError if content_length.present? && content_length > limit
 
       if charset.nil?
